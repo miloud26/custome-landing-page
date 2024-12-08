95494a3ee9af017d86e9ff0b2347a96736d6f544
miloud boudjellal <miloud00@protonmail.com>
2024-12-08 17:44:44 +0100

    add price to 5900

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     id: "l",
     name: "أونصومبل ليكات و سروال",
-    price: 3700,
+    price: 4300,
     clr: ["Noir", "Marron"],
     size: [42, 44, 46, 48],
     descImag:
