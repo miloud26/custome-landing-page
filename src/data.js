b2fc316b76a0e9fa600d8be3b269fb2103989912
miloud boudjellal <miloud00@protonmail.com>
2024-12-09 06:24:55 +0100

    add price to 5900

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const data = [
     id: "h",
     name: "أونصومبل ليكات و سروال",
     price: 3900,
-    clr: ["Noir", "Marron", "Bleu Nuit"],
+    clr: ["Noir", "Marron"],
     size: [42, 44, 46, 48],
     descImag:
       "https://res.cloudinary.com/dsqon0kae/image/upload/v1733504948/screencapture-127-0-0-1-5500-index-html-2024-12-06-18_07_40_ddayqr.webp",
@@ -25,12 +25,12 @@ export const data = [
     id: "hi",
     name: "أونصومبل ليكات و سروال",
     price: 4300,
-    clr: ["Noir", "Marron", "Bleu Nuit"],
+    clr: ["Noir", "Marron"],
     size: [42, 44, 46, 48],
     descImag:
-      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733504948/screencapture-127-0-0-1-5500-index-html-2024-12-06-18_07_40_ddayqr.webp",
+      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505177/marron-noir_p1ssmm.webp",
     themImg:
-      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505255/bluei_xzmmbn.webp",
+      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505194/marron_ba94tl.webp",
   },
   {
     id: "hih",
