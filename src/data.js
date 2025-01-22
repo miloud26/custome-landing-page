34381b9c14f091e87358a9c62076a698178a384e
miloud boudjellal <miloud00@protonmail.com>
2025-01-22 12:47:35 +0100

    new sire fix

--- src/data.js
+++ src/data.js
@@ -6,13 +6,13 @@ export const data = [
     delevery: 600,
     clr: ["Noir", "Marron", "Bleu nuit"],
     size: [42, 44, 46, 48],
-    /*descImag:
+   descImag:
       "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505177/marron-noir_p1ssmm.webp",
     themImg:
-      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505194/marron_ba94tl.webp",*/ descImag:
+      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505194/marron_ba94tl.webp", /*descImag:
       "https://res.cloudinary.com/dsqon0kae/image/upload/v1733504948/screencapture-127-0-0-1-5500-index-html-2024-12-06-18_07_40_ddayqr.webp",
     themImg:
-      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505255/bluei_xzmmbn.webp",
+      "https://res.cloudinary.com/dsqon0kae/image/upload/v1733505255/bluei_xzmmbn.webp"*/,
   },
   {
     id: "l-2",
